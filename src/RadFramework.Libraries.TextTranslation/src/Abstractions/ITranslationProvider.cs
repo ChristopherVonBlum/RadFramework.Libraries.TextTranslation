ac576c22e485bf48ce793985a5252b3bfb92818e
Christopher von Blum <christopher.vonblum@gmail.com>
2020-12-20 06:57:19 +0100

    add Translate overload to interface

--- src/RadFramework.Libraries.TextTranslation/src/Abstractions/ITranslationProvider.cs
+++ src/RadFramework.Libraries.TextTranslation/src/Abstractions/ITranslationProvider.cs
@@ -4,6 +4,7 @@ namespace RadFramework.Libraries.TextTranslation.Abstractions
 {
     public interface ITranslationProvider
     {
+        string Translate(string key);
         string Translate(string key, CultureInfo cultureInfo);
     }
 }
